40757520fef1124b58d98fbc292c4ff015dcaa01
Josh Bruce <josh@8fold.pro>
2021-04-21 10:25:02 -0500

    fix #5
    Allowing dev stability for Shoop - updated to latest version of Shoop

--- tests/AbbreviationTest.php
+++ tests/AbbreviationTest.php
@@ -23,18 +23,18 @@ class AbbreviationTest extends TestCase
             "external_link" => ["open_in_new_window" => true]
         ], $environment);
 
-        $path = Shoop::string(__DIR__)->plus("/short-doc.md");
+        $path = Shoop::this(__DIR__)->append("/short-doc.md");
         $markdown = file_get_contents($path);
         $expected = '<p><abbr title="United States Web Design System">USWDS</abbr></p>'."\n".'<p><a rel="noopener noreferrer" target="_blank" href="https://8fold.pro">External link check</a></p>'."\n";
         $actual = $converter->convertToHtml($markdown);
         $this->assertEquals($expected, $actual);
 
-        $path = Shoop::string(__DIR__)->divide("/")
-            ->dropLast()->plus("readme.html")->join("/")->start("/");
+        $path = Shoop::this(__DIR__)->divide("/")
+            ->dropLast()->append(["readme.html"])->asString("/");
         $expected = file_get_contents($path);
 
-        $path = Shoop::string(__DIR__)->divide("/")
-            ->dropLast()->plus("README.md")->join("/")->start("/");
+        $path = Shoop::this(__DIR__)->divide("/")
+            ->dropLast()->append(["README.md"])->asString("/");
         $markdown = file_get_contents($path);
 
         $actual = $converter->convertToHtml($markdown);
