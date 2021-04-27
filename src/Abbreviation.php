c9004580b72a8459c286dbbc739e55e56cf69480
Matei Stanca <i@ambientimpact.com>
2021-04-27 16:02:42 -0400

    Added methods to get and set the abbreviation and title.

--- src/Abbreviation.php
+++ src/Abbreviation.php
@@ -16,6 +16,46 @@ class Abbreviation extends AbstractInline
         $this->title = $title;
     }
 
+    /**
+     * Get the abbreviation, e.g. "HTML".
+     *
+     * @return string
+     */
+    public function getAbbreviation(): string
+    {
+        return $this->abbr;
+    }
+
+    /**
+     * Set the abbreviation, e.g. "HTML".
+     *
+     * @param string $abbr
+     */
+    public function setAbbreviation(string $abbr): void
+    {
+        $this->abbr = $abbr;
+    }
+
+    /**
+     * Get the full form of the abbreviation, e.g "HyperText Markup Language".
+     *
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the full form of the abbreviation, e.g "HyperText Markup Language".
+     *
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
     public function isContainer(): bool
     {
         return true;
