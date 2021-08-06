a18fa9deb45546cbcdb9842c621c43e5b8c67f63
Josh Bruce <josh@8fold.pro>
2021-08-06 14:52:10 -0500

    abbreviation attributes test

--- tests/AbbreviationAttributesTest.php
+++ tests/AbbreviationAttributesTest.php
@@ -4,8 +4,9 @@ namespace Eightfold\CommonMarkAbbreviations\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-use League\CommonMark\Environment;
-use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\MarkdownConverter;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\Event\DocumentParsedEvent;
 use League\CommonMark\Extension\ExternalLink\ExternalLinkExtension;
 
@@ -18,36 +19,45 @@ class AbbreviationAttributesTest extends TestCase
 {
     public function testParser()
     {
-        $environment = Environment::createCommonMarkEnvironment();
-        $environment->addExtension(new AbbreviationExtension());
-        $environment->addExtension(new ExternalLinkExtension());
-        $environment->addEventListener(DocumentParsedEvent::class, function (
-            DocumentParsedEvent $event
-        ) {
-            $document = $event->getDocument();
-            $walker = $document->walker();
-            while ($event = $walker->next()) {
-                $node = $event->getNode();
-
-                // Ignore any nodes that aren't Abbreviation nodes, and only act
-                // when we first encounter/enter an Abbreviation node.
-                if (!($node instanceof Abbreviation) || !$event->isEntering()) {
-                    continue;
-                }
-
-                // Add a test attribute. It's also possible to alter the
-                // existing attributes here.
-                $node->data['attributes']['data-event-attribute'] = 'hello';
-            }
-        });
-        $converter = new CommonMarkConverter([
+        $config = [
             "external_link" => ["open_in_new_window" => true]
-        ], $environment);
+        ];
+
+        $environment = (new Environment($config))
+            ->addExtension(new CommonMarkCoreExtension())
+            ->addExtension(new AbbreviationExtension())
+            ->addExtension(new ExternalLinkExtension())
+            ->addEventListener(DocumentParsedEvent::class, function (
+                DocumentParsedEvent $event
+            ) {
+                $document = $event->getDocument();
+                $walker = $document->walker();
+                while ($event = $walker->next()) {
+                    $node = $event->getNode();
+
+                    // Ignore any nodes that aren't Abbreviation nodes, and only act
+                    // when we first encounter/enter an Abbreviation node.
+                    if (!($node instanceof Abbreviation) || !$event->isEntering()) {
+                        continue;
+                    }
+
+                    // Add a test attribute. It's also possible to alter the
+                    // existing attributes here.
+                    $attributes = $node->data->get("attributes");
+                    $attributes['data-event-attribute'] = 'hello';
+                }
+            });
+
+        $converter = new MarkdownConverter($environment);
 
         $path = Shoop::this(__DIR__)->append("/short-doc-attributes.md");
+
         $markdown = file_get_contents($path);
+
         $expected = '<p><abbr title="United States Web Design System" data-inline-attribute="hello" data-event-attribute="hello">USWDS</abbr></p>'."\n".'<p><a rel="noopener noreferrer" target="_blank" href="https://8fold.pro">External link check</a></p>'."\n";
-        $actual = $converter->convertToHtml($markdown);
+
+        $actual = $converter->convertToHtml($markdown)->getContent();
+
         $this->assertEquals($expected, $actual);
 
         $path = Shoop::this(__DIR__)->divide("/")
